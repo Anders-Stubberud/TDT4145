c25ac151ed20bd2e1373426623e029f0a7e3d8b2
Anders Stubberud <andersvs02@gmail.com>
2024-03-21 00:35:33 +0100

    SQL-ifiser oppgave 4

--- oppgaver/oppgave_4.py
+++ oppgaver/oppgave_4.py
@@ -1,15 +1,27 @@
-from utils import query, insert, insert_return_rowID
+from utils import query
+
+'''
+Her skal du implementere et Pythonprogram (med bruk av SQL) som tar inn
+en dato og skriver ut hvilke forestillinger som finnes på denne datoen og lister
+opp hvor mange billetter (dvs. stoler) som er solgt. Ta også med forestillinger
+hvor det ikke er solgt noen billetter.
+'''
 
 def forestiller_på_dato():
 
     dato = input('Hvilken dato for forestillinger og billetter? Format: YYYY-MM-DD, eksempel: 2024-02-03   ')
-    data = query(f'SELECT forestillingID, navnPaStykke FROM Forestilling WHERE dato = "{ dato }"')
-    forestillingNavnTilIDMapper = {}
-    for forestillingID, forestillingNavn in data:
-        count = query(f'SELECT COUNT(*) FROM Billett WHERE forestillingID = { forestillingID }')[0][0]
-        forestillingNavnTilIDMapper[forestillingNavn] = forestillingNavnTilIDMapper.get(forestillingNavn, 0) + count
+
+    data = query(f'''
+        SELECT f.navnPaStykke, COUNT(b.forestillingID) AS antall_solgte_billetter
+        FROM Forestilling f
+        LEFT JOIN Billett b ON f.forestillingID = b.forestillingID
+        WHERE f.dato = "{dato}"
+        GROUP BY f.forestillingID, f.navnPaStykke
+    ''')
+
     print(f'På datoen { dato } har vi følgende forestillinger:')
-    for forestilling, antall_solgte_billetter in forestillingNavnTilIDMapper.items():
-        print(f'{forestilling}: {antall_solgte_billetter}')
+    for forestillingNavn, antall_solgte_billetter in data:
+        print(f'{forestillingNavn}, med {antall_solgte_billetter} solgte billetter')
 
 forestiller_på_dato()
+
