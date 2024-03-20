a587528c68ebb8e293648d6d053153ae9dd0a78e
dagkandPC <dagkand@ntnu.no>
2024-03-20 19:21:39 +0100

    change utils

--- oppgaver/utils.py
+++ oppgaver/utils.py
@@ -9,21 +9,11 @@ Denne filen gir tilgang til databaseoperasjonene "query" og "insert", som henhol
 '''
 Leser ut og returnerer spesifisert data.
 '''
-def query(table, select_statement="*", where_statement=None, group_by=None, having=None, order_by=None, join=None, distinct=False):
+def query(query):
     con = sqlite3.connect('./database/database.db')
-    cursor = con.cursor()
-    sql_query = f"SELECT {'' if not distinct else 'DISTINCT'} {select_statement} FROM {table}"
-    if join:
-        sql_query += f" {join}"
-    if where_statement:
-        sql_query += f" WHERE {where_statement}"
-    if group_by:
-        sql_query += f" GROUP BY {group_by}"
-    if having:
-        sql_query += f" HAVING {having}"
-    if order_by:
-        sql_query += f" ORDER BY {order_by}"
-    result = cursor.execute(sql_query).fetchall()
+    cursor = con.cursor()   
+    result = cursor.execute(query)
+    result = result.fetchall()
     con.close()
     return result
 
@@ -42,15 +32,19 @@ def insert(table, columns, values):
     con.commit()
     con.close()
 
+def insert_return_rowID(table, columns, values):
+    con = sqlite3.connect('./database/database.db')
+    cursor = con.cursor()
+    placeholders = ', '.join(['?' for _ in columns])
+    cursor.execute(f'INSERT INTO {table} ({", ".join(columns)}) VALUES ({placeholders})', values)
+    rowID = cursor.lastrowid
+    con.commit()
+    con.close()
+    return rowID
+
 # insert('Teatersal', ['salnavn'], ['Gamle scenek'])
 
 # insert('Rolle', ('navn', 'navnPaStykke'), ('verdi1', 'verdi2'))
-
-
-
-
-
-
 
 
 
