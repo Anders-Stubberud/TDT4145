9986d7dcffe807000f99357a192f3789c67388ff
barothei <barothei@stud.ntnu.no>
2024-03-21 17:34:34 +0100

    Changed formatting on oppgave 5

--- oppgaver/oppgave_5.py
+++ oppgaver/oppgave_5.py
@@ -8,10 +8,39 @@ skuespiller og rolle
 '''
 # stykke, skuespiller rolle
 
+# def skuespillere_i_stykker():
+#     data = query(f'SELECT navnPaStykke, fornavn, etternavn, navn FROM Teaterstykke NATURAL JOIN Rolle NATURAL JOIN SpillerRolle NATURAL JOIN Skuespiller')
+#     for navn_på_stykke, fornavn, etternavn, rolle_navn in data:
+#         print(f'{ fornavn } { etternavn } spiller { rolle_navn } i { navn_på_stykke }')
+
+
 def skuespillere_i_stykker():
-    data = query(f'SELECT navnPaStykke, fornavn, etternavn, navn FROM Teaterstykke NATURAL JOIN Rolle NATURAL JOIN SpillerRolle NATURAL JOIN Skuespiller')
+    data = query('SELECT navnPaStykke, fornavn, etternavn, navn FROM Teaterstykke NATURAL JOIN Rolle NATURAL JOIN SpillerRolle NATURAL JOIN Skuespiller ORDER BY navnPaStykke')
+
+    # Organize data by play
+    stykker_med_skuespillere = {}
     for navn_på_stykke, fornavn, etternavn, rolle_navn in data:
-        print(f'{ fornavn } { etternavn } spiller { rolle_navn } i { navn_på_stykke }')
+        fullt_navn = f'{fornavn} {etternavn}'
+        if navn_på_stykke not in stykker_med_skuespillere:
+            stykker_med_skuespillere[navn_på_stykke] = []
+        stykker_med_skuespillere[navn_på_stykke].append((fullt_navn, rolle_navn))
+
+    # Prepare and print the header
+    header = f'{"Forestilling":<35} {"Skuespiller":<25} {"Rolle":<30}'
+    print(header)
+    print('-' * len(header))
+
+    # Print the organized data with spacing between different plays
+    plays = list(stykker_med_skuespillere.keys())
+    for i, stykke in enumerate(plays):
+        for j, (skuespiller, rolle) in enumerate(stykker_med_skuespillere[stykke]):
+            if j == 0:
+                # For the first actor of each play, add a newline if it's not the first play
+                if i > 0:
+                    print()  # Adds a newline for spacing between plays
+                print(f'{stykke:<35} {skuespiller:<25} {rolle:<30}')
+            else:
+                print(f'{" ":<35} {skuespiller:<25} {rolle:<30}')
 
 # skuespillere_i_stykker()
 
